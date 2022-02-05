876cef52c4ef980917b73b30db08449b72d6f6c8
NikXpro <briffanathan781@gmail.com>
2022-02-05 11:32:34 +0100

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name "nth_carJack"
+description "Advanced carjack systéme"
+author "NikX#1717"
+version "0.0.1"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
